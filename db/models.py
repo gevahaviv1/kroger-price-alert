990dbd0057ba1d67e12e9670efa8373f3ece45a3
Geva Haviv <geva98@gmail.com>
2025-04-21 14:33:32 +0300

    RD-248: Add SQLAlchemy model for storing alert check results.

---
+++ db/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class PriceAlertResult(db.Model):
+    __tablename__ = 'price_alert_results'
+
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.String, nullable=False)
+    user_id = db.Column(db.String, nullable=False)
+    triggered = db.Column(db.Boolean, default=False)
+    checked_at = db.Column(db.DateTime, default=datetime.utcnow)
