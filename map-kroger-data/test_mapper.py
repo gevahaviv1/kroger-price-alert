9629f17b0bb4909de3a84e8fca8e29ca6c72a648
Geva Haviv <geva98@gmail.com>
2025-04-21 13:27:23 +0300

    RD-244: Fix file path in test_mapper to use .json extension.

--- map-kroger-data/test_mapper.py
+++ map-kroger-data/test_mapper.py
@@ -1,7 +1,7 @@
 import json
 from mapper import map_kroger_to_zenday
 
-with open("response-example.txt") as f:
+with open("response-example.json") as f:
     kroger_data = json.load(f)["data"]
 
 mapped = map_kroger_to_zenday(kroger_data)
